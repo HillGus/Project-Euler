1b7ce4a132935b719358c7e83eaf8b93a747ac9e
Gustavo Hill <guhillesheim@gmail.com>
2019-05-13 08:26:56 -0300

    Melhorando algoritmo 14

--- solutions/14 - Longest Collatz Sequence.py
+++ solutions/14 - Longest Collatz Sequence.py
@@ -1,27 +1,49 @@
 from time import process_time as pt
-
-print(pt())
+from commons import runtime
 
 generated_chains = {}
-def chain(n):
+def naive(n):
     if generated_chains.get(n) != None:
-        return generated_chains.get(n)
+        c = generated_chains.get(n)
+        return c
+        #if c[-1] != 1:
+        #    return c.extend(chain(c[-1]))
     start = [n]
     if n % 2 == 0:
         n /= 2
     else:
         n = 3 * n + 1
     if n > 1:
-        start.extend(chain(int(n)))
+        start.extend(naive(int(n)))
     generated_chains[n] = start
     return start
 
-longest_chain = 0
-longest_owner = 0
-for x in range(999999, 1, -2):
-    c = len(chain(x) + [1])
-    if c > longest_chain:
-        longest_chain = c
-        longest_owner = x
-print(longest_owner)
-print(pt())
+def new(n):
+    if generated_chains.get(n) != None:
+        c = generated_chains.get(n)
+        return c
+        if c[-1] != 1:
+            return c.extend(new(c[-1]))
+    start = [n]
+    old_n = n
+    if n % 2 == 0:
+        n /= 2
+    else:
+        n = 3 * n + 1
+    if n > 1:
+        generated_chains[old_n] = list(start)
+        start.extend(new(int(n)))
+    return start
+
+def resolve():
+    longest_chain = 0
+    longest_owner = 0
+    for x in range(999999, 1, -2):
+        c = len(new(x) + [1])
+        if c > longest_chain:
+            longest_chain = c
+            longest_owner = x
+    print(longest_owner)
+    print(sum([len(generated_chains[x]) for x in generated_chains]))
+
+runtime(resolve)
