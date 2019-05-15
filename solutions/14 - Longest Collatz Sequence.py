6645be6f2c86b8ac43a6355c0acd6ad93827bf60
Gustavo Hill <guhillesheim@gmail.com>
2019-05-15 07:58:41 -0300

    Melhorando resolução

--- solutions/14 - Longest Collatz Sequence.py
+++ solutions/14 - Longest Collatz Sequence.py
@@ -2,48 +2,52 @@ from time import process_time as pt
 from commons import runtime
 
 generated_chains = {}
-def naive(n):
+def naive_fn(n):
     if generated_chains.get(n) != None:
         c = generated_chains.get(n)
         return c
-        #if c[-1] != 1:
-        #    return c.extend(chain(c[-1]))
     start = [n]
     if n % 2 == 0:
         n /= 2
     else:
         n = 3 * n + 1
     if n > 1:
-        start.extend(naive(int(n)))
+        start.extend(naive_fn(n))
     generated_chains[n] = start
     return start
 
-def new(n):
-    if generated_chains.get(n) != None:
-        c = generated_chains.get(n)
-        return c
-        if c[-1] != 1:
-            return c.extend(new(c[-1]))
-    start = [n]
-    old_n = n
-    if n % 2 == 0:
-        n /= 2
-    else:
-        n = 3 * n + 1
-    if n > 1:
-        generated_chains[old_n] = list(start)
-        start.extend(new(int(n)))
-    return start
-
-def resolve():
+def naive_resolve():
     longest_chain = 0
     longest_owner = 0
     for x in range(999999, 1, -2):
-        c = len(new(x) + [1])
+        c = len(naive_fn(x)) + 1
         if c > longest_chain:
             longest_chain = c
             longest_owner = x
     print(longest_owner)
-    print(sum([len(generated_chains[x]) for x in generated_chains]))
 
-runtime(resolve)
+def new_fn(n):
+    if generated_chains.get(n) != None:
+        return generated_chains.get(n)
+    length = 1
+    if n % 2 == 0:
+        n /= 2
+    else:
+        n = 3 * n + 1
+    if n > 1:
+        new_gen = new_fn(n)
+        length += new_gen
+        generated_chains[n] = new_gen
+    return length + 1
+
+def new_resolve():
+    longest_chain = 0
+    longest_owner = 0
+    for x in range(999999, 1, -2):
+        c = new_fn(x)
+        if c > longest_chain:
+            longest_chain = c
+            longest_owner = x
+    print(longest_owner)
+
+runtime(new_resolve)
